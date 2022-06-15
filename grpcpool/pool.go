0e8974eb8982f2188f0243447f2ae45bd906669e
David Wartell <d@wartell.net>
2022-06-15 18:38:48 -0400

    add helper func to decode grpc errors

--- grpcpool/pool.go
+++ grpcpool/pool.go
@@ -19,7 +19,11 @@ package grpcpool
 
 import (
 	"context"
+	"github.com/pkg/errors"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"strings"
 )
 
 type PoolClientConn interface {
@@ -34,4 +38,25 @@ type Pool interface {
 type ConnectionFactory interface {
 	NewConnection(ctx context.Context) (*grpc.ClientConn, error)
 	ConnectionOk(ctx context.Context, conn *grpc.ClientConn) error
+}
+
+//goland:noinspection GoUnusedExportedFunction
+func DecodeGrpcError(err error) (connectionFailure bool, returnErr error) {
+	st, ok := status.FromError(err)
+	if ok {
+		// application specific error - Use st.Message() and st.Code()
+		if st.Code() == codes.Canceled || st.Code() == codes.Unavailable {
+			// this happens when server is gracefully shutting down with msg on client side:
+			// https://github.com/grpc/grpc-go/blob/750abe8f95cd270ab68f7298a2854148d0c33030/clientconn.go grpc: the client connection is closing
+			return true, err
+		} else {
+			rpcErr := errors.Errorf("error on grpc request %s (%d)", st.Message(), st.Code())
+			return false, rpcErr
+		}
+	} else if strings.Contains(err.Error(), "use of closed network connection") {
+		return true, err
+	} else {
+		// general GRPC error
+		return true, err
+	}
 }
