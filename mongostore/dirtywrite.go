ac51a76efe62b63ff02e9ae72dde0624c4d077dc
David Wartell <d@wartell.net>
2022-11-16 22:14:53 -0500

    comments

--- mongostore/dirtywrite.go
+++ mongostore/dirtywrite.go
@@ -37,9 +37,9 @@ var DirtyWriteError = errors.New("dirty write error")
 // Then when you update mongo:
 //
 //		filter := bson.D{
-//			{"_id", person.Id},
+//			{"_id", person.Id},	// Note: must use _id or some indexed field with a unique constraint.
 //	     	// where device.DirtyWriteGuard is 0 on new or == to the dirtyWriteGuard field of the entity we expect in the collection
-//			{"dirtyWriteGuard", person.DirtyWriteGuard},
+//			{"dirtyWriteGuard", person.DirtyWriteGuard}, // this value should be unmodified by your code as it was loaded from mongo.
 //		}
 //
 //		// increment the counter before update or insert
@@ -66,11 +66,20 @@ var DirtyWriteError = errors.New("dirty write error")
 //			return
 //		}
 //
-// In the tested and expected case mongo will return E11000 duplicate key error collection in case of dirty write. This
-// is because no document will exist that matches _id and dirtyWriteGuard causing mongo to attempt to insert a new document
-// which will return duplicate key error.
-// In case of no dirty write and no error returned by the UpdateOne() we expect either an insert (updateResult.UpsertedID
-// has a value) or an updated existing document (updateResult.MatchedCount == 1).
+// In the expected dirty write case mongo will return updateResult.MatchedCount == 0 && updateResult.UpsertedID == nil.
+// Meaning that 0 documents matched the filter with the unique id and the dirtyWriteGuard equality.
+//
+// In case of no dirty write and no error returned by the UpdateOne() we expect either an insert
+// (updateResult.UpsertedID has a value) or an updated existing document (updateResult.MatchedCount == 1).
+//
+// In the the real-world and tested case mongo will return E11000 duplicate key error collection in case of dirty write.
+// This is because no document will exist that matches _id and dirtyWriteGuard causing mongo to attempt to insert a new
+// document which will return duplicate key error.
+//
+// return E11000 duplicate key error collection in case of dirty write. This is because no document will exist that
+// matches _id and dirtyWriteGuard causing mongo to attempt to insert a new document which will return duplicate key
+// error. In case of no dirty write and no error returned by the UpdateOne() we expect either an insert
+// (updateResult.UpsertedID has a value) or an updated existing document (updateResult.MatchedCount == 1).
 //
 //goland:noinspection GoUnusedExportedFunction
 func CheckForDirtyWriteOnUpsert(updateResult *mongo.UpdateResult, inputErr error) (err error) {
@@ -137,6 +146,21 @@ func RetryDirtyWrite(dirtyWriteFunc DirtyWriteProtectedFunc) (err error) {
 	return
 }
 
+// IsDuplicateKeyError can help handle expected behavior for any mongo command that uses Upsert. If IsDuplicateKeyError
+// returns true for the error returned by a mongo operation, and you are using Upsert then you are expected to retry.
+//
+// Will return false if there are multiple nested mongo writeExceptions and one of the errors has a Code != 11000
+// (duplicate key) indicating there are other errors that should be handled and not ignored or handled the same.
+//
+// BE WARNED: Mongo can at any time return E11000 duplicate key error for ANY command with Upsert enabled. Mongo expects
+// the application to handle this error. This happens if: "During an update with upsert:true option, two (or more)
+// threads may attempt an upsert operation using the same query predicate and, upon not finding a match, the threads
+// will attempt to insert a new document. Both inserts will (and should) succeed, unless the second causes a unique
+// constraint violation." See: https://jira.mongodb.org/browse/SERVER-14322
+//
+// Note: its documented that mongo retries  under the usual cases. While that may be true im seeing it still returns an
+// error.
+//
 //goland:noinspection GoUnusedExportedFunction
 func IsDuplicateKeyError(err error) bool {
 	if err == nil {
